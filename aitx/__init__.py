c55978280da625be53774703860fbbd07a37b325
jeroyang <jeroyang@gmail.com>
2017-12-29 09:54:46 +0800

    add func show_history

--- aitx/__init__.py
+++ aitx/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from aitx.imagebox import *
-
+from aitx.showhistory import *
 import os
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
---
+++ aitx/showhistory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+def show_history(train='acc', validation=None, train_history=train_history):
+    if validation is None:
+        validation = 'val_' + train
+    plt.plot(train_history.history[train], label='train')
+    plt.plot(train_history.history[validation], label='validation')
+    plt.title('Train history')
+    plt.ylabel(train)
+    plt.xlabel('Epoch')
+    plt.legend(loc='best')
+    plt.show()
