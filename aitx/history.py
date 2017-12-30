13483d2f703167949da799d3d5b4e71d70006577
jeroyang <jeroyang@gmail.com>
2017-12-30 08:53:11 +0800

    add human level error

--- aitx/history.py
+++ aitx/history.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import matplotlib.pyplot as plt
 from collections import defaultdict
 class History:
 
     def __init__(self):
         self._history_log = []
+        self._lines = {}
 
     def update(self, history):
         self._history_log.append(history)
+
+    def add_human_accuracy(self, score):
+        self._lines['human'] = score
 
     def get_full_history(self):
         full_history = defaultdict(list)
@@ -21,10 +24,14 @@ class History:
 
     def show(self, train='acc', validation=None):
         train_history = self.get_full_history()
+        epochs = len(train_history[train])
         if validation is None:
             validation = 'val_' + train
+        for name, score in self._lines.items():
+            plt.plot([0, epochs], [score, score], linestyle='--', color='gray', label=name)
         plt.plot(train_history[train], label='train')
         plt.plot(train_history[validation], label='validation')
+
         plt.title('Train history')
         plt.ylabel(train)
         plt.xlabel('Epoch')
