355a322f0f5225d76a1ca9b50f36432e7f8ee034
jeroyang <jeroyang@gmail.com>
2017-12-30 08:54:36 +0800

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='aitx',
-    version='0.2.3',
+    version='0.3.0',
     description="The toolbox for general ML projects",
     long_description=readme,
     author="Chia-Jung, Yang",
