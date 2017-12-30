2dd4d90447a93bddc21d8c01b7c4e05d352d0fff
jeroyang <jeroyang@gmail.com>
2017-12-30 08:54:08 +0800

    add diagnosis

--- aitx/history.py
+++ aitx/history.py
@@ -6,13 +6,13 @@ class History:
 
     def __init__(self):
         self._history_log = []
-        self._lines = {}
+        self.human_accuracy = None
 
     def update(self, history):
         self._history_log.append(history)
 
-    def add_human_accuracy(self, score):
-        self._lines['human'] = score
+    def set_human_accuracy(self, score):
+        self.human_accuracy = score
 
     def get_full_history(self):
         full_history = defaultdict(list)
@@ -27,8 +27,8 @@ class History:
         epochs = len(train_history[train])
         if validation is None:
             validation = 'val_' + train
-        for name, score in self._lines.items():
-            plt.plot([0, epochs], [score, score], linestyle='--', color='gray', label=name)
+        if self.human_accuracy is not None:
+            plt.plot([0, epochs], [self.human_accuracy, self.human_accuracy], linestyle='--', color='gray', label='human')
         plt.plot(train_history[train], label='train')
         plt.plot(train_history[validation], label='validation')
 
@@ -37,3 +37,23 @@ class History:
         plt.xlabel('Epoch')
         plt.legend(loc='best')
         plt.show()
+
+    def diagnosis(self, human_accuracy=None):
+        if human_accuracy is not None:
+            self.human_accuracy = human_accuracy
+        if self.human_accuracy is None:
+            print("Need human accuracy")
+        full_history = self.get_full_history()
+        hum_error = 1 - self.human_accuracy
+        acc_error = 1 - full_history['acc'][-1]
+        val_error = 1 - full_history['val_acc'][-1]
+        template = "Human level error:\t{:.3f}\nTraining set error:\t{:.3f}\nDev/Test set error:\t{:.3f}"
+        print(template.format(hum_error, acc_error, val_error))
+        bias = max(0, acc_error - hum_error)
+        variance = val_error - acc_error
+        print('Avoidable bias:\t{:.3f}\nVariance:\t{:.3f}'.format(bias, variance))
+        if bias >= variance:
+            print('Recommend train on a larger model or more time.')
+        else:
+            print('{:->30}'.format(''))
+            print('Recommend more regulation or dropout.')
