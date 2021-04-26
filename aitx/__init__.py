d419fab815788eff4e21ed7e61d0c08f454b5273
Chia-Jung, Yang <jeroyang@gmail.com>
2021-04-26 20:00:01 +0800

    add lightbox

--- aitx/__init__.py
+++ aitx/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from aitx.imagebox import *
+from aitx.lightbox import *
 from aitx.history import *
 import os
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.4.0'
