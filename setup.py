2c59cac147557e6e7101855f03ada3edc611001b
Chia-Jung, Yang <jeroyang@gmail.com>
2021-04-26 20:26:47 +0800

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='aitx',
-    version='0.4.0',
+    version='0.4.1',
     description="The toolbox for general ML projects",
     long_description=readme,
     author="Chia-Jung, Yang",
